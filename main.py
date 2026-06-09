65c64d41d5a8deec0a63ed7b0cc2abd167293d2f
fabricioferreiraatm-cyber <fabricioferreira.atm@gmail.com>
2026-06-09 19:49:40 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-teste
+import os
+import time
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+while True:
+    try:
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+        requests.post(
+            url,
+            data={
+                "chat_id": CHAT_ID,
+                "text": "🚀 Bot funcionando!"
+            }
+        )
+
+        print("Mensagem enviada")
+
+    except Exception as erro:
+        print("ERRO:", erro)
+
+    time.sleep(900)
